cdd246e295d78ba8bb70b91b4d411a4506a71876
datamaximus <jwiram@gmail.com>
2022-01-23 13:57:40 -0700

    Add caching to sharpen middleware

--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -1,9 +1,9 @@
 import express from 'express';
 import sharpen from '../../utilties/sharpen';
+
 const images = express.Router();
 
 images.get('/', sharpen, async (req, res) => {
-    res.sendFile(`/assets/thumb/${req.query.filename}.jpg`, { root: '.' });
 });
 
 export default images;
--- src/utilties/sharpen.ts
+++ src/utilties/sharpen.ts
@@ -1,5 +1,8 @@
 import express from "express";
 import sharp from "sharp";
+import fs from "fs";
+
+const path = "./public/assets/thumb/";
 
 const sharpen = async (
   req: express.Request,
@@ -7,11 +10,31 @@ const sharpen = async (
   next: Function
 ) => {
   let filename = req.query.filename;
+  let width = req.query.width;
+  let height = req.query.height;
 
-  sharp(`./assets/full/${filename}.jpg`)
-    .resize(Number(req.query.width), Number(req.query.height))
-    .toFile(`./assets/thumb/${filename}.jpg`, function (err) {});
-  await next();
+  fs.access(
+    `${path}${filename}${width}x${height}.jpeg`,
+    fs.constants.R_OK,
+    async (err) => {
+      if (err) {
+        await sharp(`./public/assets/full/${filename}.jpeg`)
+          .resize(Number(width), Number(height))
+          .toFile(`${path}${filename}${width}x${height}.jpeg`);
+        res.sendFile(`${path}${filename}${width}x${height}.jpeg`, {
+          root: ".",
+        });
+        console.log("Resizing image");
+      } else {
+        res.sendFile(`${path}${filename}${width}x${height}.jpeg`, {
+          root: ".",
+        });
+        console.log("Loads image");
+      }
+    }
+  );
+
+  next();
 };
 
 export default sharpen;
